42ffd68a7dbd87a20ca5f9dcb3ea34b031f4220b
modev <modev.404@gmail.com>
2024-09-15 11:20:32 +0300

    update layout home and add table component

--- components/home.tsx
+++ components/home.tsx
@@ -10,17 +10,6 @@ export const dynamic = "force-dynamic";
 export default function Home() {
   return (
     <div className="flex flex-col space-y-5 bg-background text-foreground">
-      <header className="flex items-center justify-between px-6 py-4 border-b">
-        <div className="flex items-center gap-4">
-          <Music2Icon />
-        </div>
-        <div className="items-center space-x-3">
-          <ThemeToggle />
-          <Button variant="ghost" size="icon" className="w-8 h-8 rounded-full">
-            <UserIcon className="w-5 h-5" />
-          </Button>
-        </div>
-      </header>
       <Suspense fallback={<AlbumSkeleton />}>
         <LatestAlbum />
       </Suspense>

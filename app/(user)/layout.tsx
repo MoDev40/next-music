1743cb992973b52e12c551babf6485d96152d888
modev <modev.404@gmail.com>
2024-09-17 09:02:39 +0300

    install axios and configure session provider

--- app/(user)/layout.tsx
+++ app/(user)/layout.tsx
@@ -1,17 +1,20 @@
 import BottomNav from "@/components/bottom-nav";
 import TopNavBar from "@/components/topnav-bar";
 import { ReactNode } from "react";
+import { SessionProvider } from "next-auth/react";
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
-    <main className="flex flex-col min-h-screen">
-      <div className="h-16">
-        <TopNavBar />
-      </div>
-      <div className="flex-1 py-4">{children}</div>
-      <div>
-        <BottomNav />
-      </div>
-    </main>
+    <SessionProvider>
+      <main className="flex flex-col min-h-screen">
+        <div className="h-16">
+          <TopNavBar />
+        </div>
+        <div className="flex-1 py-4">{children}</div>
+        <div>
+          <BottomNav />
+        </div>
+      </main>
+    </SessionProvider>
   );
 }
